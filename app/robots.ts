969d589dea6981310083cd3c3315639ec066ce16
Sangwoo <jswoo1995@gmail.com>
2024-09-16 15:26:58 +0900

    posts폴더 최상단으로 이동

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,13 @@
-import { baseUrl } from 'app/sitemap'
+import { baseUrl } from "app/sitemap";
+import { MetadataRoute } from "next";
 
-export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-  }
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "*",
+            },
+        ],
+        sitemap: `${baseUrl}/sitemap.xml`,
+    };
 }
